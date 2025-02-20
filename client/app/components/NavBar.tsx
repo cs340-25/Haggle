01a8e341aa96daa7019043fb058dd1ac85159b83
Alex Pacheco <ap112095@gmail.com>
2025-02-20 13:22:50 -0500

    fixed some scaling stuff :D

--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div className='absolute top-0 left-0 h-[50px] flex justify-between items-center w-[100%] px-10 bg-[#1b4381] text-[#ffdebd]'>
+    <div className='absolute top-0 left-0 h-[7vh] flex justify-between items-center w-[100%] px-10 bg-[#1b4381] text-[#ffdebd] text-[3.25vh]'>
         <a href="/"><b>Haggle</b></a>
         <div className='flex gap-5'>
           <a href="/testing">Testing</a>
           <a href="/lobbies">Lobbies</a>
           <a href="/rules">Rules</a>
         </div>
-        <hr className='w-[100%] absolute mt-[50px] ml-[-35px] border-[#5c6ead]'></hr>
+        <hr className='w-[100%] absolute mt-[7vh] ml-[-5vh] border-[#5c6ead]'></hr>
     </div>
   )
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
     <div className="w-[100%] h-[100vh] flex flex-col items-center gap-5 relative bg-[#1d4325] font-impact font-black">
       <NavBar />
       
-      <div className='mt-[75px] min-h-[600px] min-w-[600px] bg-[url("/chip.svg")] bg-cover text-center pt-[325px] text-[#ffdebd] text-[45px]'>
-        <button className="bg-[#1b4381] p-[0px] pl-[10px] pr-[10px] rounded-[25px] border-4 border-[#5c6ead]">Offline</button>
+      <div className='mt-[10vh] h-[60vh] w-[60vh] bg-[url("/chip.svg")] bg-cover text-center pt-[32.5vh] text-[#ffdebd] text-[5.75vh]'>
+        <button className="bg-[#1b4381] pl-[1vh] pr-[1vh] rounded-[25px] border-[.5vh] border-[#5c6ead]">Offline</button>
       </div>
 
-      <div className="absolute flex items-center mt-[40%] min-w-[90%] text-[50px] text-[#ffdebd]">
-        <button className="bg-[#1b4381] p-[15px] pl-[75px] pr-[75px] rounded-[30px] border-4 border-[#5c6ead]">Random</button>
-        <button className="bg-[#1b4381] p-[15px] pl-[75px] pr-[75px] rounded-[30px] border-4 border-[#5c6ead] ml-[10%]">Join</button>
-        <input type="text" placeholder="Enter Join Code" className="peer bg-[#1b4381] p-[15px] rounded-[30px] border-4 border-[#5c6ead] ml-[2%] text-[30px] text-[#ffdebd]"/>
-        <button className="bg-[#1b4381] p-[15px] pl-[75px] pr-[75px] rounded-[30px] border-4 border-[#5c6ead] ml-[10%]">Host</button>
+      <div className="absolute flex items-center mt-[80vh] min-w-[90%] text-[6vh] text-[#ffdebd]">
+        <button className="bg-[#1b4381] p-[2vh] pl-[7vh] pr-[7vh] rounded-[30px] border-[.5vh] border-[#5c6ead]">Random</button>
+        <button className="bg-[#1b4381] p-[2vh] pl-[7vh] pr-[7vh] rounded-[30px] border-[.5vh] border-[#5c6ead] ml-[10vh]">Join</button>
+        <input type="text" placeholder="Enter Join Code" className="peer bg-[#1b4381] p-[2vh] rounded-[30px] border-[.5vh] border-[#5c6ead] ml-[2%] text-[4vh] text-[#ffdebd]"/>
+        <button className="bg-[#1b4381] p-[2vh] pl-[7vh] pr-[7vh] rounded-[30px] border-[.5vh] border-[#5c6ead] ml-[10vh]">Host</button>
       </div>
 
     </div>
