c3a5c2922162a0d9292e7d8ec50f36dd44e2a0e4
Alex Pacheco <ap112095@gmail.com>
2025-02-20 13:57:42 -0500

    fixed the rules page, made navbar consistent throughout

--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div className='absolute top-0 left-0 h-[7vh] flex justify-between items-center w-[100%] px-10 bg-[#1b4381] text-[#ffdebd] text-[2.5vh]'>
+    <div className='absolute top-0 left-0 h-[7vh] flex justify-between items-center w-[100%] px-10 bg-[#1b4381] text-[#ffdebd] text-[2.5vh] font-light'>
         <a href="/"><b>Haggle</b></a>
         <div className='flex gap-5'>
           <a href="/testing">Testing</a>
