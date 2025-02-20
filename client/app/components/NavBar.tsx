cad58259386e2ec07138f71950ac511519f5dfd5
cnie109 <christian.niehaus000@gmail.com>
2025-02-19 19:55:43 -0500

    add rules page

--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div className='absolute top-0 left-0 h-[50px] flex justify-between items-center w-[100%] px-10 bg-neutral-800'>
+    <div className='absolute top-0 left-0 h-[50px] flex justify-between items-center w-[100%] px-10 bg-[#1b4381] text-[#ffdebd]'>
         <a href="/"><b>Haggle</b></a>
         <div className='flex gap-5'>
           <a href="/testing">Testing</a>
           <a href="/lobbies">Lobbies</a>
+          <a href="/rules">Rules</a>
         </div>
+        <hr className='w-[100%] absolute mt-[50px] ml-[-35px] border-[#5c6ead]'></hr>
     </div>
   )
 }
---
+++ client/app/rules/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import GameServer from '../components/GameServer';
+
+const page = () => {
+  return (
+    <div className='w-full h-full flex flex-col items-center justify-center relative gap-5 overflow-scroll bg-[#1d4325]'>
+        <NavBar />
+
+        <div className='mt-[75px] min-h-[500px] min-w-[500px] bg-[url("/chip.svg")] bg-cover text-center pt-[250px] font-impact font-black text-[#1b4381] text-[75px]'>
+        Rules
+        </div>
+        <hr style={{color:"#FFFFFF", minWidth:"75%", minHeight:"20px"}}></hr>
+        <div className="bg-[#1b4381] w-[75%] rounded-[20px] border-[#5c6ead] border-2">
+          <div className='text-center text-[20px] p-[5%] text-[#ffdebd] font-black '>
+          <span className='text-6xl'>How to Play </span>
+          <br></br><br></br>
+          <span className='text-4xl'>The Auction Stage:</span>
+          <br></br>
+  At the start of each round the deck is shuffled and each player draws 5 cards and begin the Haggle stage. 
+  <br></br><br></br>
+  <span className='text-4xl'>The Haggle Stage:</span><br></br>
+  Players will determine if they will or won't haggle. upon both players determining if they will or wont haggle, haggling players will show their hand, and shuffle all cards except for their lowest card, into the deck and draw 4 cards. Players then Move on to the Bidding Stage.<br></br>
+  <br></br>
+  <span className='text-4xl'>The Bidding Stage:</span><br></br>
+  Both players select a card from their hand and places it face down, after both players place cards face down they flip the cards and the player with the bigger card wins that bid. The cards in the bid are then placed aside to no longer be in play for the rest of the game. This continues until all cards have been played, at which point the player who won the most bids wins the round. The deck is then shuffled, and both players draw a new hand and start another Haggle stage. The first player to win 2 rounds wins.<br></br>
+  <br></br>
+  <span className='text-4xl'>Card Rules:</span><br></br>
+  Ace is valued at 1, unless against a face card at which point it is treated as 14<br></br>
+  Face cards are valued as follows: Jack = 11, Queen = 12, King = 13<br></br>
+  During Haggling, Ace is treated as a 1<br></br>
+  In the event of 2 cards with the same value being played the greater suit will win. (Club > Diamond > Spade > Heart)<br></br>
+
+          </div>
+        </div>
+        <div className='h-[50%] text-[100px]'><br></br></div>
+    </div>
+  )
+}
+
+export default page;
