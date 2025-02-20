4d68c8c860e5a1387b3449845214137b8adc267d
Alex Pacheco <ap112095@gmail.com>
2025-02-20 13:36:19 -0500

    fixed scaling *again*

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
         <button className="bg-[#1b4381] pl-[1vh] pr-[1vh] rounded-[25px] border-[.5vh] border-[#5c6ead]">Offline</button>
       </div>
 
-      <div className="absolute flex items-center mt-[80vh] min-w-[90%] text-[6vh] text-[#ffdebd]">
-        <button className="bg-[#1b4381] p-[2vh] pl-[7vh] pr-[7vh] rounded-[30px] border-[.5vh] border-[#5c6ead]">Random</button>
-        <button className="bg-[#1b4381] p-[2vh] pl-[7vh] pr-[7vh] rounded-[30px] border-[.5vh] border-[#5c6ead] ml-[10vh]">Join</button>
-        <input type="text" placeholder="Enter Join Code" className="peer bg-[#1b4381] p-[2vh] rounded-[30px] border-[.5vh] border-[#5c6ead] ml-[2%] text-[4vh] text-[#ffdebd]"/>
-        <button className="bg-[#1b4381] p-[2vh] pl-[7vh] pr-[7vh] rounded-[30px] border-[.5vh] border-[#5c6ead] ml-[10vh]">Host</button>
+      <div className="absolute flex items-center ml-[5.5vw] mt-[80vh] min-w-[90%] text-[3vw] text-[#ffdebd]">
+        <button className="bg-[#1b4381] p-[2vh] pl-[3vw] pr-[3vw] rounded-[30px] border-[.5vh] border-[#5c6ead]">Random</button>
+        <button className="bg-[#1b4381] p-[2vh] pl-[3vw] pr-[3vw] rounded-[30px] border-[.5vh] border-[#5c6ead] ml-[5vw]">Join</button>
+        <input type="text" placeholder="Enter Join Code" className="peer bg-[#1b4381] p-[2vh] rounded-[30px] border-[.5vh] border-[#5c6ead] ml-[2%] text-[2vw] text-[#ffdebd]"/>
+        <button className="bg-[#1b4381] p-[2vh] pl-[3vw] pr-[3vw] rounded-[30px] border-[.5vh] border-[#5c6ead] ml-[5vw]">Host</button>
       </div>
 
     </div>
